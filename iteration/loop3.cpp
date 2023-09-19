7f8f57171a6215cdf35385b17a5eee887bfa87db
Glen <138078342+forodark@users.noreply.github.com>
2023-09-19 18:40:35 +0800

    commit iteration exercises

---
+++ iteration/loop3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char input;
+    cout << "Enter any, X to end: \n";
+    for(;input != 'X';) {
+        cin >> input;
+    }
+}
---
+++ iteration/loop4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "Enter Scores, -1 to end: \n";
+    for(int input = 0;input != -1; cin >> input) {}
+}
