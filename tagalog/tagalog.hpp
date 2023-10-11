4c42419544357bf7b3a3df515c5b16bcf72b904b
Glen <138078342+forodark@users.noreply.github.com>
2023-10-11 19:16:18 +0800

    got bored lmao

---
+++ tagalog/tagalog.hpp
@@ -0,0 +1,30 @@
+#ifndef TAGALOG_HPP
+#define TAGALOG_HPP
+
+#define ginagamit using
+#define pangalangpatlang namespace
+#define clabas cout
+#define cpasok cin
+#define taposlinya endl
+#define kung if
+#define kundi else
+#define balik return
+#define pangunahin main
+#define intidyer int
+#define lutang float
+#define doble double
+#define tali string
+#define karakter char
+#define kunin gets
+#define para for
+#define habang while
+#define totoo true
+#define kasinungalingan false
+#define sirain break
+#define ituloy continue
+
+#endif //TAGALOG_HPP
+
+
+
+
