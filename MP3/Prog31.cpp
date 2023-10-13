b413cb61977978655267a9494957df05214b057e
Glen <138078342+forodark@users.noreply.github.com>
2023-10-13 17:23:46 +0800

    mp3

--- MP3/Prog31.cpp
+++ MP3/Prog31.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace gl;
 
-#define SIZE 2
+#define SIZE 10
 
 //Using Arrays, create a complete C++ program that will accept 10 of your classmates’ 
 //names and their last semester's GWA. The program should sort the list based on the highest GWA.
@@ -13,13 +13,12 @@ struct Student {
     double gwa;
 };
 
-
 void insertionSort(Student arr[]) {
     for (int i = 1; i < SIZE; i++) {
         Student key = arr[i];
         int j = i - 1;
 
-        while (j >= 0 && arr[j].gwa < key.gwa) {
+        while (j >= 0 && arr[j].gwa > key.gwa) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
@@ -33,7 +32,7 @@ int main() {
 
         for (int i = 0; i < SIZE; i++) {
             students[i].name = getString("[" + to_string(i) + "] Enter student name: ");
-            students[i].gwa = getDouble("[" + to_string(i) + "] Enter student GWA: ");
+            students[i].gwa = getDouble("[" + to_string(i) + "] Enter student GWA: ", 1.00, 5.00);
         }
 
         insertionSort(students); 
