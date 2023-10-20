e3407039717a1d4e1a7f37d9c7636bc298a24122
Glen <138078342+forodark@users.noreply.github.com>
2023-10-20 16:33:47 +0800

    started glenlib 2.2 dev

---
+++ glenlib/v2.2 INDEV/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "glenlib.hpp"
+
+using namespace std;
+using namespace gl;
+
+
+int main() {
+    println("Hello World");
+    print("Hello", "World", 1, 2);
+
+    return 0;
+}
